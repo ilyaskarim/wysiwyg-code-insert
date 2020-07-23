49e750dda7a011761335dbfeadf517a453a75320
ilyaskarim <ilyas.datoo@gmail.com>
2020-07-23 09:42:36 +0500

    Changes

--- src/js/app.js
+++ src/js/app.js
@@ -24,12 +24,39 @@ let wysiwygCodeInsert = {
       python: "python",
     },
     ruby: {
-      ruby: "ruby"
+      ruby: "ruby",
     },
     java: {
-      java: "java"
+      java: "java",
     },
   },
-  init: () => {},
-  template: () => {},
+  codeMirrorFiles: {
+    css: {
+      main: '<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/codemirror/5.56.0/codemirror.min.css" />',
+    },
+    js: {
+      main: '<script src="https://cdnjs.cloudflare.com/ajax/libs/codemirror/5.56.0/codemirror.min.js"></script>',
+    },
+  },
+  init(el) {
+    let _this = this;
+    let pre = document.querySelector(el)
+    // document.head.append(this.codeMirrorFiles.css.main)
+    // document.head.append(this.codeMirrorFiles.js.main)
+    // document.querySelector(el).append(pre);
+    editor = CodeMirror(document.querySelector("pre"), {
+      mode: "xml",
+      theme: "ayu-mirage",
+      height: "200px",
+      extraKeys: { "Ctrl-Space": "autocomplete" },
+      value: "<button>Click Me</button>",
+    });
+    editor.setSize("auto", "auto");
+    pre.onclick = () => {
+      _this.generateDropdown();
+    };
+  },
+  generateDropdown() {
+    alert("will generate dropdown");
+  },
 };

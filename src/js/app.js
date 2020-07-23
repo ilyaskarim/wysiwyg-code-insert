1a9c91e944c50b4535a9c9e2258cad817320f127
ilyaskarim <ilyas.datoo@gmail.com>
2020-07-23 06:58:47 +0500

    Initial sestup

---
+++ src/js/app.js
@@ -0,0 +1,35 @@
+let wysiwygCodeInsert = {
+  languageTypes: {
+    xml: {
+      html: "html",
+      xml: "xml",
+    },
+    css: {
+      less: "less",
+      css: "css",
+      scss: "scss",
+      sass: "sass",
+    },
+    js: {
+      typescript: "typescript",
+      javascript: "javascript",
+    },
+    sql: {
+      sql: "sql",
+    },
+    php: {
+      php: "php",
+    },
+    python: {
+      python: "python",
+    },
+    ruby: {
+      ruby: "ruby"
+    },
+    java: {
+      java: "java"
+    },
+  },
+  init: () => {},
+  template: () => {},
+};

e25b48393cc506738ed93200e0b5afe07396a216
ilyas <ilyas.datoo@gmail.com>
2020-07-25 13:40:20 +0500

    Added all themes to demo.html

--- src/js/app.js
+++ src/js/app.js
@@ -1,133 +1,105 @@
 let wysiwygCodeInsert = {
-  languageTypes: {
-    xml: {
-      html: "html",
-      xml: "xml",
-    },
-    css: {
-      less: "less",
-      css: "css",
-      scss: "scss",
-      sass: "sass",
-    },
-    js: {
-      typescript: "typescript",
-      javascript: "javascript",
-    },
-    sql: {
-      sql: "sql",
-    },
-    php: {
-      php: "php",
-    },
-    python: {
-      python: "python",
-    },
-    ruby: {
-      ruby: "ruby",
-    },
-    java: {
-      java: "java",
-    },
+  defaults: {
+    mode: "xml",
+    theme: "ayu-mirage"
   },
   options: {
-    html: `<div class="options-container">
+    languageTypes: ["html", , "xml", "less", "css", "scss", "sass", "typescript", "javascript", "sql", "php", "python", "ruby", "java"],
+    html() {
+      return `<div class="options-container">
     <div class="select-language">
       <div class="input-dropdown">
         <select class="language" type="text">
-  
-          <img src="https://image.flaticon.com/icons/svg/60/60995.svg" alt="">
-  
-          <option hidden disabled selected value>Select Language</option>
-          <option value="">HTML</option>
-          <option value="">Less</option>
-          <option value="">CSS</option>
-          <option value="">Python</option>
-          <option value="">JavaScript</option>
+          <img src="https://image.flaticon.com/icons/svg/60/60995.svg" alt="" />
+          ${this.languageTypes.map((c) => {
+            return `<option>${c}</option>`
+          })}
         </select>
-      </div>
+      </div>  
     </div>
   
     <div class="select-language">
       <div class="input-dropdown">
         <select class="theme" type="text">
-          <img src="https://image.flaticon.com/icons/svg/60/60995.svg" alt="">
+          <img src="https://image.flaticon.com/icons/svg/60/60995.svg" alt="" />
           <option hidden disabled selected value>Select Theme</option>
           <option>3024-day</option>
-    <option>3024-night</option>
-    <option>abcdef</option>
-    <option>ambiance</option>
-    <option>ayu-dark</option>
-    <option>ayu-mirage</option>
-    <option>base16-dark</option>
-    <option>base16-light</option>
-    <option>bespin</option>
-    <option>blackboard</option>
-    <option>cobalt</option>
-    <option>colorforth</option>
-    <option>darcula</option>
-    <option>dracula</option>
-    <option>duotone-dark</option>
-    <option>duotone-light</option>
-    <option>eclipse</option>
-    <option>elegant</option>
-    <option>erlang-dark</option>
-    <option>gruvbox-dark</option>
-    <option>hopscotch</option>
-    <option>icecoder</option>
-    <option>idea</option>
-    <option>isotope</option>
-    <option>lesser-dark</option>
-    <option>liquibyte</option>
-    <option>lucario</option>
-    <option>material</option>
-    <option>material-darker</option>
-    <option>material-palenight</option>
-    <option>material-ocean</option>
-    <option>mbo</option>
-    <option>mdn-like</option>
-    <option>midnight</option>
-    <option>monokai</option>
-    <option>moxer</option>
-    <option>neat</option>
-    <option>neo</option>
-    <option>night</option>
-    <option>nord</option>
-    <option>oceanic-next</option>
-    <option>panda-syntax</option>
-    <option>paraiso-dark</option>
-    <option>paraiso-light</option>
-    <option>pastel-on-dark</option>
-    <option>railscasts</option>
-    <option>rubyblue</option>
-    <option>seti</option>
-    <option>shadowfox</option>
-    <option>solarized dark</option>
-    <option>solarized light</option>
-    <option>the-matrix</option>
-    <option>tomorrow-night-bright</option>
-    <option>tomorrow-night-eighties</option>
-    <option>ttcn</option>
-    <option>twilight</option>
-    <option>vibrant-ink</option>
-    <option>xq-dark</option>
-    <option>xq-light</option>
-    <option>yeti</option>
-    <option>yonce</option>
-    <option>zenburn</option>
+          <option>3024-night</option>
+          <option>abcdef</option>
+          <option>ambiance</option>
+          <option>ayu-dark</option>
+          <option selected >ayu-mirage</option>
+          <option>base16-dark</option>
+          <option>base16-light</option>
+          <option>bespin</option>
+          <option>blackboard</option>
+          <option>cobalt</option>
+          <option>colorforth</option>
+          <option>darcula</option>
+          <option>dracula</option>
+          <option>duotone-dark</option>
+          <option>duotone-light</option>
+          <option>eclipse</option>
+          <option>elegant</option>
+          <option>erlang-dark</option>
+          <option>gruvbox-dark</option>
+          <option>hopscotch</option>
+          <option>icecoder</option>
+          <option>idea</option>
+          <option>isotope</option>
+          <option>lesser-dark</option>
+          <option>liquibyte</option>
+          <option>lucario</option>
+          <option>material</option>
+          <option>material-darker</option>
+          <option>material-palenight</option>
+          <option>material-ocean</option>
+          <option>mbo</option>
+          <option>mdn-like</option>
+          <option>midnight</option>
+          <option>monokai</option>
+          <option>moxer</option>
+          <option>neat</option>
+          <option>neo</option>
+          <option>night</option>
+          <option>nord</option>
+          <option>oceanic-next</option>
+          <option>panda-syntax</option>
+          <option>paraiso-dark</option>
+          <option>paraiso-light</option>
+          <option>pastel-on-dark</option>
+          <option>railscasts</option>
+          <option>rubyblue</option>
+          <option>seti</option>
+          <option>shadowfox</option>
+          <option>solarized dark</option>
+          <option>solarized light</option>
+          <option>the-matrix</option>
+          <option>tomorrow-night-bright</option>
+          <option>tomorrow-night-eighties</option>
+          <option>ttcn</option>
+          <option>twilight</option>
+          <option>vibrant-ink</option>
+          <option>xq-dark</option>
+          <option>xq-light</option>
+          <option>yeti</option>
+          <option>yonce</option>
+          <option>zenburn</option>
         </select>
       </div>
     </div>
     <div class="actions">
       <div class="delete-icon">
-        <img class="delete" src="https://image.flaticon.com/icons/svg/3159/3159662.svg" alt="">
+        <img class="delete" src="https://image.flaticon.com/icons/svg/3159/3159662.svg" alt="" />
       </div>
       <div class="save-icon">
-        <img class="delete" src="https://image.flaticon.com/icons/svg/60/60731.svg" alt="">
+        <img class="delete" src="https://image.flaticon.com/icons/svg/60/60731.svg" alt="" />
       </div>
     </div>
   </div>
-  `,
+  
+  `;
+    },
   },
   codeMirrorFiles: {
     css: {
@@ -168,13 +140,16 @@ let wysiwygCodeInsert = {
     let pre = document.querySelector("pre");
     if (pre.classList.contains("code-insert-initialized") === false) {
       let div = document.createElement("div");
-      let html = this.options.html;
+      let html = this.options.html();
       div.innerHTML = html;
       div.classList.add("wysiwyg-code-insert-options");
       let deleteIcon = div.querySelector(".delete-icon");
       let saveIcon = div.querySelector(".save-icon");
       let languageSelection = div.querySelector("select.language");
       let themeSelection = div.querySelector("select.theme");
+      languageSelection.onchange = (e) => {
+        editor.setOption("mode", e.target.options[e.target.selectedIndex].textContent);
+      };
       themeSelection.onchange = (e) => {
         editor.setOption("theme", e.target.options[e.target.selectedIndex].textContent);
       };
